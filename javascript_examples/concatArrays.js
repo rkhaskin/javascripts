3496ebdcb5ad907003cecaaf6bb5a00bbde4976e
Slavik <rkhaskin@yahoo.com>
2018-08-02 22:25:07 -0400

    updated

--- javascript_examples/concatArrays.js
+++ javascript_examples/concatArrays.js
@@ -20,7 +20,18 @@ console.log("concatenanted array1 and array2", newArray);
 
 // concat creates a new array consisting of the elements in the object on which it is called, 
 // followed in order by, for each argument, the elements of that argument (if the argument is an array) 
-// or the argument itself (if the argument is not an array). 
+// or the argument itself (if the argument is not an array). Concat recognizes arrays and adds their elements individually, but only for 
+// the first level
 var ar3 = ['a', 'b'].concat(1, 2, [3], [4,5]);
 console.log("concatenanted args and arrays", ar3);
+/* expected output
+concatenanted args and arrays [ 'a', 'b', 1, 2, 3, 4, 5 ]
+*/
+
+var ar4 = ['a', 'b'].concat(1, 2, [3], [[4,5]]);
+console.log("concatenanted args and arrays", ar4);
+/* expected output
+concatenanted args and arrays [ 'a', 'b', 1, 2, 3, [ 4, 5 ] ] - only one level is removed on [[4,5]]
+*/
+
 
