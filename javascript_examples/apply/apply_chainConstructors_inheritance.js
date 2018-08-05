3774c331516cd320ad3d0e8ca4d4f6b22aafec93
Slavik <rkhaskin@yahoo.com>
2018-08-04 23:29:40 -0400

    updated

--- javascript_examples/apply/apply_chainConstructors_inheritance.js
+++ javascript_examples/apply/apply_chainConstructors_inheritance.js
@@ -1,8 +1,16 @@
 require('../../prototype/FunctionProtoConstruct');
 
 function MyConstructor() {
-    // call() converts args to array
-    console.log('inside MyConstructor', [].slice.call(arguments));
+    // call() converts args to array. As per mozilla, slice prevents optimization 
+    // by V8.: var args = [].slice.call(arguments));
+    // use Array.apply to get arguments as array
+    var args = (arguments.length === 1 ? [arguments[0]] : Array.apply(null, arguments));
+    console.log(args);
+    /* print each argument
+    for (var i = 0; i < arguments.length; i++) {
+        console.log('argument ' + i + '= ', arguments[i]);
+    }
+    */
 
     // build properties from each element of the array
     for (var nProp = 0; nProp < arguments.length; nProp++) {
