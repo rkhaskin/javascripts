465c86feea98a47ad5092ea27c6698d2a164e229
Rostislav Khaskin <rkhaskin@yahoo.com>
2018-08-19 21:14:13 -0400

    added call filter

--- javascript_examples/call/call_filter.js
+++ javascript_examples/call/call_filter.js
@@ -1,10 +1,19 @@
 var original = "There is 1 number 123 hello 431.";
 
-// execute filter on original
+//console.log([].map.call(original, function() {
+
+// }));
+// execute filter on the array. call() converts a string into an array-like structure
+// T,h,e,r,e ..... and then the filter() works on individual args
 var updated = Array.prototype.filter.call(original, function(val) {
+    //console.log(val);
     return val.match(/1/);
 });
-
+// same code as above
+var updated = [].filter.call(original, function(val) {
+    console.log(val);
+    return val.match(/1/);
+});
 // Outputs: ["1","1","1"]
 console.log(updated);
 
